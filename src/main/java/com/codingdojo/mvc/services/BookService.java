04b1f62b2de80dc0ae9f6be21aeaaa9d51619318
imalexkim <alexander_kim4@yahoo.com>
2021-02-17 16:41:36 -0800

    uad assingment

---
+++ src/main/java/com/codingdojo/mvc/services/BookService.java
@@ -0,0 +1,58 @@
+package com.codingdojo.mvc.services;
+
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.codingdojo.mvc.models.Book;
+import com.codingdojo.mvc.repositories.BookRepository;
+
+@Service
+public class BookService {
+	private final BookRepository bookRepository;
+	
+	public BookService(BookRepository bookRepository) {
+		this.bookRepository = bookRepository;	
+	}
+	
+	  // returns all the books
+    public List<Book> allBooks() {
+        return bookRepository.findAll();
+    }
+    
+    
+    // creates a book
+    public Book createBook(Book b) {
+        return bookRepository.save(b);
+    }
+    // retrieves a book
+    public Book findBook(Long id) {
+        Optional<Book> optionalBook = bookRepository.findById(id);
+        if(optionalBook.isPresent()) {
+            return optionalBook.get();
+        } else {
+         return null;
+        }
+    }
+     // Updates a book   
+        public Book updateBook(Long id, String title, String desc, String lang, Integer numOfPages) {
+        	Book book = findBook(id);
+        	book.setTitle(title);
+        	book.setDescription(desc);
+        	book.setLanguage(lang);
+        	book.setNumberOfPages(numOfPages);
+        	return bookRepository.save(book);	
+        }
+        
+        //Deletes a book
+        public void deleteBook(Long id) {
+        	bookRepository.deleteById(id);
+        }
+   
+    }
+	
+	
+
+
